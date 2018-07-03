8b72cf3b0759da50d8a675f6c92bd81de61feafc
dinanjana <dinanjanagunaratne@gmail.com>
2018-07-03 17:30:54 +0530

    fix erros on getting result from samples

--- tensoflowjs-test/src/index.js
+++ tensoflowjs-test/src/index.js
@@ -1,12 +1,16 @@
 import 'babel-core/register';
 import 'babel-polyfill';
-import { average, runSample } from './sample.js';
+import { average, runSample, printNumberOfTensors } from './sample.js';
+import { getExcelAsJSON } from './process';
 
   const runSamples = async () => {
     const samples = await Promise.all([runSample(), average()])
                             .catch((e) => { console.error('Error occureed', e) });
-    console.log(`Sample 1 ------->predict -------->${samples[0].print()}`);
-    console.log(`Sample 2 ------->tidy -----------> ${samples[1].print()}`);
+    console.log(`Sample 1 ------->predict -------->${samples[0]}`);
+    console.log(`Sample 2 ------->tidy -----------> ${samples[1]}`);
+    printNumberOfTensors();
   };
+
+  //getExcelAsJSON();
 
   runSamples().catch(e => { console.error('Unhandled exception', e) });
--- tensoflowjs-test/src/sample.js
+++ tensoflowjs-test/src/sample.js
@@ -1,31 +1,33 @@
 import * as tf from '@tensorflow/tfjs';
 
-  // Define a model for linear regression.
-  const model = tf.sequential();
-  model.add(tf.layers.dense({units: 1, inputShape: [1]}));
 
-  // Prepare the model for training: Specify the loss and the optimizer.
-  model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
-
-  // Generate some synthetic data for training.
-  const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);   
-  const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
-
-  // Train the model using the data.
-  model.fit(xs, ys).then(() => {
-    // Use the model to do inference on a data point the model hasn't seen before:
-    model.predict(tf.tensor2d([5], [1, 1])).print();
-  });
 
   const runSample = () => {
-     return model.fit(xs, ys).then(() => {
+    console.log('numTensors (start): ');
+    printNumberOfTensors();
+    // Define a model for linear regression.
+    const model = tf.sequential();
+    model.add(tf.layers.dense({units: 1, inputShape: [1]}));
+
+    // Prepare the model for training: Specify the loss and the optimizer.
+    model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
+
+    // Generate some synthetic data for training.
+    const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
+    const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
+
+    // Train the model using the data.
+    return model.fit(xs, ys).then(() => {
       // Use the model to do inference on a data point the model hasn't seen before:
-      model.predict(tf.tensor2d([5], [1, 1]));
+      return model.predict(tf.tensor2d([5], [1, 1]));
     });
-  }
+  };
 
   // tf.tidy takes a function to tidy up after
   const average = () => {
+    console.log('numTensors (before tidy):');
+    printNumberOfTensors();
+    return tf.tidy(() => {
       // tf.tidy will clean up all the GPU memory used by tensors inside
       // this function, other than the tensor that is returned.
       //
@@ -34,11 +36,27 @@ import * as tf from '@tensorflow/tfjs';
       // put your math ops in a tidy!
       const y = tf.tensor1d([1.0, 2.0, 3.0, 4.0]);
       const z = tf.ones([4]);
-
+      console.log('numTensors (in tidy): ' + tf.memory().numTensors);
       return y.sub(z).square().mean();
+    });
   }; // Output: 3.5
+
+  const printNumberOfTensors = () => {
+    console.log('numTensors: ' + tf.memory().numTensors);
+  };
+  const linearRegressionSample = async () => {
+    const model = tf.sequential();
+    model.add(tf.layers.dense({units: 1, inputShape: [1]}));
+    model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
+
+    const xs = tf.tensor1d([1, 2, 3, 4, 5, 6.4, 8, 8.2, 8.9]);
+    const ys = tf.tensor1d([0, -1, -2, -5, 0 ,1 ,3, 7, 8]);
+
+    await model.fit(xs, ys)
+  };
 
   export {
     runSample,
     average,
+    printNumberOfTensors,
   }
